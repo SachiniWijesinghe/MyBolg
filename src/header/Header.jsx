467f76276c9793f1d80f9008ccb7ade5be16eb83
Sachini Sathsarani Wijesinghe <it20641092@my.sliit.lk>
2023-02-17 12:16:39 +0530

    adding header

---
+++ src/header/Header.jsx
@@ -0,0 +1,18 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="head">
+      <div className="headerTITLES">
+        <div className="headerTitles">
+
+          <span className="headerTitlesm">React & Node</span>
+          <span className="headerTitleLg">Blog</span>
+         
+        </div>
+      </div>
+      <img className="headerImage" src="https://c.wallhere.com/photos/4e/9a/1500x859_px_Canada_forest_lake_landscape_mist_mountain_nature-703656.jpg!d" alt=""></img>
+
+    </div>
+  )
+}
