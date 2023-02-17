75559d5929ad667dc0a4ac6996762aeb4595bf5b
Sachini Sathsarani Wijesinghe <it20641092@my.sliit.lk>
2023-02-17 12:16:27 +0530

    adding top bar

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import TopBar from "./topbar/TopBar";
+import Home from "./pages/home/Home";
+
+function App() {
+  return (
+    <>
+    <TopBar/>
+   
+    <Home/>
+
+    </>
+    
+    // <div className="App">
+    //   blog app
+    // </div>
+  );
+}
+
+export default App;
